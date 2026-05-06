f590385159590c6eb524b229822b2646bab5e525
czarjoti <egb154@gmail.com>
2026-05-06 19:59:45 -0400

    modified UNSW init

--- intro_datasci_utils/unsw.py
+++ intro_datasci_utils/unsw.py
@@ -7,20 +7,12 @@ from scipy.sparse import issparse
 from numpy import ndarray
 
 class UNSW(Dataset):
-    testing_url = "https://unsw-my.sharepoint.com/personal/z5025758_ad_unsw_edu_au/_layouts/15/download.aspx?UniqueId=2a810f6a%2Dcc3d%2D4d98%2D909e%2D37489d8daf98"
-    training_url = "https://unsw-my.sharepoint.com/personal/z5025758_ad_unsw_edu_au/_layouts/15/download.aspx?UniqueId=49413d38%2D3330%2D4358%2Dbfa2%2D0349031198a5"
-
-    def __init__(self, transformer=None) -> None:
+    def __init__(self, file, transformer=None) -> None:
         super().__init__()
-        url: str
 
         train = transformer is None
-        if train:
-            url = self.training_url
-        else:
-            url = self.testing_url
 
-        df = pd.read_csv(url)
+        df = pd.read_csv(file)
         X = df.drop(['id', 'attack_cat', 'label'], axis=1)
         self.y = df['label'].values
 
