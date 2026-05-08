d1e3aca17d527a8b78ce706dd50530644ab8a145
czarjoti <egb154@gmail.com>
2026-05-08 18:23:14 -0400

    updated feature extraction

--- intro_datasci_utils/feature_extraction.py
+++ intro_datasci_utils/feature_extraction.py
@@ -1,7 +1,7 @@
 from sklearn.feature_selection import mutual_info_classif, SelectPercentile
 from sklearn.preprocessing import LabelEncoder
 
-def extract_features(X, y, symbolic):
+def extract_features(X, y, symbolic, percentage):
             ex_X = X.copy()
             for s in symbolic:
                 ex_X[s] = LabelEncoder().fit_transform(ex_X[s]) # type: ignore
@@ -9,7 +9,7 @@ def extract_features(X, y, symbolic):
             discrete_features = [col in symbolic for col in ex_X.columns]
             percent = SelectPercentile(
                 score_func=lambda X, y: mutual_info_classif(X, y, discrete_features=discrete_features),
-                percentile=50
+                percentile=percentage
             )
 
             p = percent.fit(ex_X, y)  # type: ignore
